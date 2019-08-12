806df5377453c1e7788d89ba63903d794e3164aa
ChaiUrs <mysorechaitra@gmail.com>
2019-08-12 00:42:12 +0000

    Trees methods - vampire assignment

--- vampire.js
+++ vampire.js
@@ -1,3 +1,4 @@
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,24 +11,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    let familyLength = this.offspring.length;
+    return familyLength;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let currentVamp = this;
+    let vampCount = 0;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      vampCount++;
+    }
+    return vampCount;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    for(let i = 0; i < this.numberOfOffspring; i++) {
+      if(vampire.name === this.offspring[i].name) {
+        return true;
+      }
+    }
+    return false;
   }
-
+      
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
